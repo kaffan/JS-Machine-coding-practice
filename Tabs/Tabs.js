bf5b9d68b961a1008d4918686b7450357a8d7281
Affan Khan <kaffan022@gmail.com>
2025-08-03 18:14:31 +0530

    git ignore added

---
+++ Tabs/Tabs.js
@@ -0,0 +1,39 @@
+function main() {
+  const tabsContainer = document.querySelector(".tabs");
+  console.log(tabsContainer);
+  const tab1 = tabsContainer?.children[0];
+  const tab2 = tabsContainer?.children[1];
+  const tab3 = tabsContainer?.children[2];
+  const box1 = document?.getElementById("box1");
+  const box2 = document?.getElementById("box2");
+  const box3 = document?.getElementById("box3");
+  tab1?.addEventListener("click", () => {
+    console.log(1);
+    setTabStyle(tab1, tab2, tab3);
+    return setStyle(box1, box2, box3);
+  });
+  tab2?.addEventListener("click", () => {
+    setTabStyle(tab2, tab1, tab3);
+    return setStyle(box2, box1, box3);
+  });
+  tab3?.addEventListener("click", () => {
+    setTabStyle(tab3, tab2, tab1);
+    return setStyle(box3, box2, box1);
+  });
+}
+const setTabStyle = (tab1, tab2, tab3) => {
+    tab1.style.backgroundColor = "#5d63f7";
+    tab1.style.color = "white";
+    tab3.style.color = "black";
+    tab2.style.backgroundColor = "white";
+    tab2.style.color = "black";
+    tab3.style.backgroundColor = "white";
+}
+const setStyle = (box1, box2, box3) => {
+  console.log(box1, box2, box3);
+  box1.style.visibility = "visible";
+  box2.style.visibility = "hidden";
+  box3.style.visibility = "hidden";
+};
+
+main();
